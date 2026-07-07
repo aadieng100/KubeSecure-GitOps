a00ab82056132077632eb6866f1ca88192c634d3
abdouladieng <diengabdoulaziz110@gmail.com>
2026-07-07 23:37:13 +0000

    feat(tf-compute): deploy hardened aws eks cluster and managed node group with private endpoints

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "cluster_role_arn" {
+  type = string
+}
+
+variable "node_role_arn" {
+  type = string
+}
